99d54a960113e0375d0e517509b82c3dbd5ff72b
Joaquin Muñoz <joaquinmurap@gmail.com>
2022-11-08 15:42:00 -0500

    1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -36,7 +36,9 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  var suma = 2 + 1;
+ var x = 2
+ var y = 2
+  var suma = x + y;
   return suma;
 }
 
