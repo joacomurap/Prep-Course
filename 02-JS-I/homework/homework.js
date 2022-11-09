2dcb5b32cdd454eff3ab5d696c8856759fa7ed49
Joaquin Muñoz <joaquinmurap@gmail.com>
2022-11-08 20:24:58 -0500

    commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,18 +28,15 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  var hola = str;
-  return hola;
+  var devolverString = "Hola";
+  return devolverString;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
- var x = 2
- var y = 2
-  var suma = x + y;
-  return suma;
+
 }
 
 function resta(x, y) {
